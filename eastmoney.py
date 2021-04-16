a6d987eb9f23247fff34dc3fdc8d8fbfbf1db813
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2021-04-16 11:26:36 +0800

    Add getPageSize

--- eastmoney.py
+++ eastmoney.py
@@ -1,11 +1,11 @@
+import os
 import time
+import math
 import json
 import requests
 import pymongo
 import datetime
 import pandas as pd
-
-PAGE = 223
 
 data = []
 
@@ -15,32 +15,41 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
 }
 
+PATTERN = 'http://58.push2.eastmoney.com/api/qt/clist/get?pn={}&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1618489972043'
+
+
+def createFolder(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
 
 def saveJson(data):
-    filename = './snowball/em{}.json'.format(str(datetime.datetime.now().date()))
+    createFolder('./snowball')
+    filename = './snowball/em{}.json'.format(
+        str(datetime.datetime.now().date()))
     with open(filename, 'w') as file_obj:
         json.dump(data, file_obj)
         print(len(data), 'items saved')
 
 
-def fetch(page):
+def fetch():
+    page = getPageSize()
+    print(page,'pages to fetch')
     for i in range(1, page):
-        # print(i)
-        pattern = 'http://58.push2.eastmoney.com/api/qt/clist/get?pn={}&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1618489972043'
-        url = pattern.format(i)
+        url = PATTERN.format(i)
         raw = requests.get(url, headers=headers).json()['data']
         total = raw['total']
         stockList = raw['diff']
-        # print(total)
-        # print(len(stockList))
         data.extend(stockList)
         print('|', end='', flush=True)
         time.sleep(2)
-    #     print('|', end='', flush=True)
-    #     time.sleep(2)
-    # print()
     print()
     saveJson(data)
 
 
-fetch(PAGE)
+def getPageSize():
+    url = PATTERN.format(1)
+    total = requests.get(url, headers=headers).json()['data']['total']
+    return math.ceil(total/20)+1
+
+fetch()
