96c0cec7e68978a9782fafdb0c4f7071e1e04d6a
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2021-04-27 17:35:20 +0800

    flask post

---
+++ hi.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import dao
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+
+@app.route('/data/<symbol>', methods=['POST', 'GET'])
+def data(symbol):
+    data = []
+    res = dao.names.find({'symbol': symbol})
+    return res[0]
+
+
+@app.route('/student', methods=['post'])
+def add_stu():
+    if not request.data:  # 检测是否有数据
+        return ('fail')
+    student = request.data.decode('utf-8')
+    student_json = json.loads(student)
+    return request.data
+
+
+if __name__ == '__main__':
+    app.run(port=8086, debug=True)
