9a18add381f44bc770628b73dd79b23c42cc3f8b
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2021-04-30 17:10:24 +0800

    Add ma5

--- ma_n.py
+++ ma_n.py
@@ -6,20 +6,54 @@ import tools
 quoteDao = dao.quotes
 nameDao = dao.names
 
-start = "2018-01-01"
-PERIOD = 250
+START = "2018-01-01"
+PERIOD = 8
 COMMISSION = 2.5 / 10000
+principal = 100.0
+window = []
 
 
-def preLoad():
-    results = []
+def go(start, period):
+    global principal, PERIOD, window
+    if period > 0:
+        if len(window) < 6:
+            oneDay = {}
+            res = quoteDao.find({'date': start})
+            for quote in res:
+                oneDay[quote['symbol']] = quote['close']
+            window.append(oneDay)
+        else:
+            print(window)
+        go(find.nextTradingDay(start), period - 1)
+
+
+def goOne():
+    global principal
+    serie = []
+    window = []
+    n = 5
+    res = quoteDao.find({'symbol': 'SZ000006'}).sort('date', 1)
+    for quote in res:
+        item = {'date': quote['date'], 'close': quote['close']}
+        if len(serie) > 5:
+            sum = quote['close']
+            for i in range(1, n):
+                sum = sum + serie[-i]['close']
+            avg = sum / n
+            item['ma5'] = avg
+        else:
+            item['ma5'] = quote['close']
+        serie.append(item)
+
     cnt = 0
-    total = nameDao.count_documents({})
-    print('Items to search:', total)
-    for name in nameDao.find():
+    for i in range(1, len(serie) - 1):
+        if (serie[i - 1]['close'] < serie[i - 1]['ma5'] and serie[i]['close'] > serie[i]['ma5']):
+            cnt = cnt + 1
+            print(cnt, serie[i]['date'], serie[i]['close'], serie[i]['ma5'])
+            principal = principal / serie[i]['close'] * serie[i + 1]['close']
+            # print(serie)
+    print(principal)
 
 
 if __name__ == '__main__':
-    # merge()
-    # getPE('SH601318')
-    preLoad()
+    goOne()
