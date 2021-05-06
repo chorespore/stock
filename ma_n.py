ec3b9f3b7e95bc2d1a8fac0475a72d6747a69caf
翟广超 <zhaiguangchao@cmict.chinamobile.com>
2021-05-06 17:04:44 +0800

    Add ma6

--- ma_n.py
+++ ma_n.py
@@ -13,6 +13,8 @@ COMMISSION = 2.5 / 10000
 principal = 100.0
 window = []
 
+dayBoard = {}
+
 
 def go(start, period):
     global principal, PERIOD, window
@@ -29,14 +31,18 @@ def go(start, period):
 
 
 def goOne(symbol):
+    global dayBoard
     principal = 100.0
     serie = []
+
     y = []
-    n = 5
+    n = 6
+    closeSum = 1
+    highSum = 1
     res = quoteDao.find({'symbol': symbol}).sort('date', 1)
     for quote in res:
-        item = {'date': quote['date'], 'close': quote['close']}
-        if len(serie) > 5:
+        item = {'symbol': symbol, 'date': quote['date'], 'close': quote['close'], 'high': quote['high']}
+        if len(serie) > n:
             sum = quote['close']
             for i in range(1, n):
                 sum = sum + serie[-i]['close']
@@ -47,34 +53,56 @@ def goOne(symbol):
         serie.append(item)
 
     cnt = 0
-    for i in range(1, len(serie) - 1):
+    passCnt = 0
+    for i in range(1, len(serie) - 2):
         if (serie[i - 1]['close'] < serie[i - 1]['ma5'] and serie[i]['close'] > serie[i]['ma5']):
             cnt = cnt + 1
-            principal = principal / serie[i]['close'] * serie[i + 1]['close']
+            if(dayBoard.__contains__(serie[i]['date']) == False):
+                dayBoard[serie[i]['date']] = []
+            dayBoard[serie[i]['date']].append(serie[i]['symbol'])
+            if(serie[i]['close'] * 1.09 < serie[i + 1]['high']):
+                principal = principal / serie[i]['close'] * serie[i]['close'] * 1.09 * (1 - COMMISSION)
+                passCnt = passCnt + 1
+            else:
+                principal = principal / serie[i]['close'] * serie[i + 1]['close'] * (1 - COMMISSION)
+            closeSum = closeSum + serie[i]['close']
+            highSum = highSum + serie[i + 1]['high']
             # print(cnt, serie[i]['date'], serie[i]['close'], format(principal, '0.2f'))
             # print(serie)
             # y.append(principal)
     # print('Earnings:', principal)
     # find.draw(range(len(y)), y)
-    return principal, cnt
+    # print(highSum, closeSum)
+    return principal, cnt, highSum / closeSum
 
 
 def goAll():
+    stop = 0
     res = []
     data = []
     start = time.time()
     stocks = nameDao.find()
     total = nameDao.count_documents({})
     for stock in stocks:
-        p, days = goOne(stock['symbol'])
-        res.append(p)
+        p, days, passCnt = goOne(stock['symbol'])
         # tools.showProgress(len(res), total, start)
         # print(stock['name'], p, end='\t')
         if days != 0:
-            print(len(res), stock['name'], format(p, '0.2f'), days)
-            item = {'name': stock['name'], 'symbol': stock['symbol'], 'earning': format(p, '0.2f'), 'days': days}
+            res.append(p)
+            print(len(res), stock['name'], format(p, '0.2f'), passCnt, days)
+            item = {'name': stock['name'], 'symbol': stock['symbol'], 'earning': format(p, '0.2f'), 'pass': passCnt, 'days': days}
             data.append(item)
-    tools.saveCSV(data, './snowball/ma5A.csv')
+        stop = stop + 1
+        if stop > 1000:
+            break
+    tools.saveCSV(data, './snowball/ma5.csv')
+
+    # dayData = []
+    # for key, value in dayBoard.items():
+    #     dayItem = {'date': key, 'number': len(value)}
+    #     dayData.append(dayItem)
+    # tools.saveCSV(dayData, './snowball/ma5-cnt.csv')
+
     return res
 
 
